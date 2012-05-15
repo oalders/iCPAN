d57f9b66806cb13bbce20e21f1c93c4619af62cb
Olaf Alders <olaf@wundersolutions.com>
2012-05-14 21:15:06 -0500

    Adds everything to populate_db.sh which is required for a nightly build.

--- perl/bin/populate_db.sh
+++ perl/bin/populate_db.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+minicpan
+
 perl bin/icpan.pl --action insert_authors
 perl bin/icpan.pl --action insert_distributions
 perl bin/icpan.pl --action insert_modules
 perl bin/icpan.pl --action pod_by_dist
-rm ../iCPAN.sqlite.zip
-zip ../iCPAN.sqlite.zip ../iCPAN.sqlite
+perl bin/finish_db
+
+cd ..
+mv iCPAN.sqlite.zip iCPAN.sqlite.zip.old
+zip iCPAN.sqlite.zip iCPAN.sqlite
