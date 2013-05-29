40f6d8704eee17e23ffe8bfe007217817cba9e47
Olaf Alders <olaf@wundersolutions.com>
2013-05-29 09:31:22 -0400

    Run minicpan via Carton.

--- perl/bin/populate_db.sh
+++ perl/bin/populate_db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#minicpan
+bin/carton minicpan
 
 bin/carton bin/icpan.pl --action insert_authors
 bin/carton bin/icpan.pl --action insert_distributions
@@ -9,5 +9,4 @@ bin/carton bin/icpan.pl --action pod_by_dist
 bin/carton bin/icpan.pl --action finish_db
 
 cd ..
-mv iCPAN.sqlite.zip iCPAN.sqlite.zip.old
 zip iCPAN.sqlite.zip iCPAN.sqlite
